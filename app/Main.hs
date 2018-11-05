a609e10038ed1e4208bda5716528ea97567cae75
Max Grechko <gremax@gremax.me>
2018-11-05 18:58:31 +0200

    Add simple network connection

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
 import Lib
+import Network
+import System.IO
+
+server = "irc.freenode.org"
+port   = 6667
 
 main :: IO ()
-main = someFunc
+main = do
+    h <- connectTo server (PortNumber (fromIntegral port))
+    hSetBuffering h NoBuffering
+    t <- hGetContents h
+    print t
