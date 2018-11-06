5dfb101e50e556246026bbf42a9ea24163dbb4d7
Max Grechko <gremax@gremax.me>
2018-11-06 19:29:36 +0200

    Add communication with server

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,30 @@ module Main where
 import Lib
 import Network
 import System.IO
+import Text.Printf
 
-server = "irc.freenode.org"
-port   = 6667
+channel  = "#gremax"
+nickname = "dudebot"
+port     = 6667
+server   = "irc.freenode.org"
 
 main :: IO ()
 main = do
-    h <- connectTo server (PortNumber (fromIntegral port))
-    hSetBuffering h NoBuffering
-    t <- hGetContents h
-    print t
+    handle <- connectTo server (PortNumber (fromIntegral port))
+    hSetBuffering handle NoBuffering
+    write handle "NICK" nickname
+    write handle "USER" (nickname ++ " 0 * :dude bot")
+    write handle "JOIN" channel
+    listen handle
+
+write :: Handle -> String -> String -> IO ()
+write handle action command = do
+    hPrintf handle "%s %s\r\n" action command
+    printf         "> %s %s\n" action command
+
+listen :: Handle -> IO ()
+listen handle = forever $ do
+    output <- hGetLine handle
+    putStrLn output
+  where
+    forever a = do a; forever a
