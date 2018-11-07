41cdd00e8fb4582ebccf68524c4fcb4e13ff7beb
Max Grechko <gremax@gremax.me>
2018-11-07 19:04:58 +0200

    Move functions to Lib module

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,30 @@
 module Lib
-    ( someFunc
+    ( listen
+    , start
+    , write
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Network
+import System.IO
+import Text.Printf
+
+start :: String -> String -> String -> Int -> IO ()
+start nickname channel server port = do
+    handle <- connectTo server (PortNumber (fromIntegral port))
+    hSetBuffering handle NoBuffering
+    write handle "NICK" nickname
+    write handle "USER" (nickname ++ " 0 * :dude bot")
+    write handle "JOIN" channel
+    listen handle
+
+listen :: Handle -> IO ()
+listen handle = forever $ do
+    output <- hGetLine handle
+    putStrLn output
+  where
+    forever a = do a; forever a
+
+write :: Handle -> String -> String -> IO ()
+write handle action command = do
+    hPrintf handle "%s %s\r\n" action command
+    printf         "> %s %s\n" action command
